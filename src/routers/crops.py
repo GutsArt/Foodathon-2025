5cefb6c1f0b114d13c2ab71a958cf9b78f41ab85
artem <artemgudc@gmail.com>
2025-10-28 21:22:39 +0100

    Add web/example.html

--- src/routers/crops.py
+++ src/routers/crops.py
@@ -4,7 +4,7 @@ from services.ecocrop_service import get_crop
 router = APIRouter()
 
 @router.get("/")
-def check_crop(name: str = Query(..., description="Название культуры (например, tomato)")):
+def check_crop(name: str = Query(..., description="Название культуры (carrot, okra, onion, Wheat, Maize , Papaya, Pineapple, Sugarcane,  Coffee, )")): # tomato, cabbage,
     result = get_crop(name=name)
     if not result:
         return {"error": f"Культура '{name}' не найдена"}
